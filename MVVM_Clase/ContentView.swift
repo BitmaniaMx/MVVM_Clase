99f558ef76c2513a55437847aa8014a39bcf7dbd
Rafael <rafael.glez@gmail.com>
2024-08-23 16:11:54 -0600

    Initial Commit

---
+++ MVVM_Clase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM_Clase
+//
+//  Created by Rafael Gonzalez on 23/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVM_Clase/MVVM_ClaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_ClaseApp.swift
+//  MVVM_Clase
+//
+//  Created by Rafael Gonzalez on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_ClaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
