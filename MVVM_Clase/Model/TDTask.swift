abef05d856ae0146cd0394abd40f2f824990b26f
Rafael <rafael.glez@gmail.com>
2024-08-25 18:11:15 -0600

    Ejemplo de uso MVVM con SwuiftUI

---
+++ MVVM_Clase/Model/TDTask.swift
@@ -0,0 +1,8 @@
+//
+//  TDTask.swift
+//  MVVM_Clase
+//
+//  Created by Rafael Gonzalez on 23/08/24.
+//
+
+import Foundation
---
+++ MVVM_Clase/ViewModel/TDTaskListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TDTaskListViewModel.swift
+//  MVVM_Clase
+//
+//  Created by Rafael Gonzalez on 23/08/24.
+//
+
+import Foundation
